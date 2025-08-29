2cf16a109396ba0517299c156eb3073539fadae1
SRIRAM <srirammurugesan1807@gmail.com>
2025-08-29 10:07:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,13 @@ import './../styles/App.css';
 const App = () => {
   return (
     <div>
+     <p>Enter your name:</p>
+     <input
+       type="text"
+       value={name}
+       onChange={(e) => setName(e.target.value)}
+     />
+     {name ? <p>Hello {name}!</p> : null}
         {/* Do not remove the main div */}
     </div>
   )
